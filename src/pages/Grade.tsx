52bffa46ebef9810144d5b264495522d1fc28a27
IngeborgOlssoen <iskaareolssoen@gmail.com>
2025-07-15 13:28:45 +0200

    Fix: put navbar to the bottom

--- src/pages/Grade.tsx
+++ src/pages/Grade.tsx
@@ -4,10 +4,15 @@ import GradeButton from "../components/GradeButton";
 
 function Grade() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <Navbar />
-      <h1 className="text-center py-16">Velg trinn</h1>
-      <GradeButton />
+      
+      <div className="flex-1 flex flex-col justify-center">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl md:text-4xl font-bold text-center mb-8 md:mb-12">Velg trinn</h1>
+          <GradeButton />
+        </div>
+      </div>
 
       <Footer />
     </div>
