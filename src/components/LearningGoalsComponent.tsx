e6fb9762c20b8beb0fd13e896bcd9fa98d34ecb7
ingridtr <ingridtryan@gmail.com>
2025-07-16 14:21:01 +0200

    test

--- src/components/LearningGoalsComponent.tsx
+++ src/components/LearningGoalsComponent.tsx
@@ -20,7 +20,7 @@ function LearningGoalsComponent({ goals }: LearningGoalsComponentProps) {
           style={{ width: "60vw" }}
           onClick={() => handleLearningGoalsClick(goal)}
         >
-          <h3 className="text-lg font-semibold">Kompetansemål {index + 1}</h3>
+          <h3 className="text-lg font-semibold">hei {index + 1}</h3>
           <p className="text-black">{goal}</p>
         </div>
       ))}
